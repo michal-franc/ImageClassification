84adcd0130be2c19cffdaaba59d698a2bd9509f0
LaM <lam.michal.franc@gmail.com>
2011-01-04 20:25:25 +0100

    - little refactorization of Naive Bayes calculations , added Tests Project

--- ImageRecognition/ClassyfiAlgorithm/NaiveBayes.cs
+++ ImageRecognition/ClassyfiAlgorithm/NaiveBayes.cs
@@ -12,21 +12,13 @@ namespace ImageRecognition.ClassyfiAlgorithm
         public int Classify(IContinuousDistribution gen1,IContinuousDistribution gen2, double p1, double p2, List<double> klasyfikowanyObiekt)
         {
 
-            double val1 = p1 * CalculateDensity(klasyfikowanyObiekt[0],gen1);
-            double val2 = p2 * CalculateDensity(klasyfikowanyObiekt[0], gen2);
+            double val1 = p1 * gen1.Density(klasyfikowanyObiekt[0]);
+            double val2 = p2 * gen2.Density(klasyfikowanyObiekt[0]);
 
             if (val1 > val2)
                 return 1;
             else
                 return 2;
-        }
-
-        private double CalculateDensity(double x,IContinuousDistribution generator)
-        {
-            double value = 1.0 / (generator.StdDev * Math.Sqrt(2 * Math.PI));
-            double value1 = -Math.Pow(x - generator.Mean, 2) / (2 * generator.Variance);
-            double value2 = Math.Exp(value1);
-            return value *value2;
         }
 
         public double CalculateBayesRisk(IContinuousDistribution gen1,IContinuousDistribution gen2,double p1,double p2)
@@ -35,56 +27,78 @@ namespace ImageRecognition.ClassyfiAlgorithm
             double x = 0.0;
             double sum = 0.0;
             double sum1 = 0.0;
+
+
+            found = FindCommonPoint(gen1, gen2, ref x);
+
             if (gen2.Mean > gen1.Mean)
             {
-                for (double i = gen1.Mean; i < gen2.Mean; i += 0.001)
-                {
-                    if (Math.Abs(gen1.Density(i) - gen2.Density(i)) < 0.00001)
-                    {
-                        x = i;
-                        found = true;
-                        break;
-                    }
-                }
 
                 if (found)
                 {
-                    for (double i = x; i < gen1.Mean + (4 * gen1.StdDev); i += 0.1)
-                    {
-                        sum += 0.1 * gen1.Density(i);
-                    }
-
-                    for (double i = x - (4 * gen2.StdDev); i < x; i += 0.1)
-                    {
-                        sum1 += 0.1 * gen2.Density(i);
-                    }
+                    return CalculateIntegral(gen1, gen2, x, p1, p2);
                 }
 
             }
             else
             {
-                for (double i = gen2.Mean; i < gen1.Mean; i += 0.001)
+
+                if (found)
                 {
-                    if (Math.Abs(gen1.Density(i) - gen2.Density(i)) < 0.00001)
+                    return CalculateIntegral(gen2, gen1, x, p1, p2);
+                }
+            }
+
+            return 0.0;
+        }
+
+        //Find the common point beetwen 2 distributions
+        private bool FindCommonPoint(IContinuousDistribution dist1,IContinuousDistribution dist2,ref double commonPoint)
+        {
+            bool found = false;
+            if (dist2.Mean > dist1.Mean)
+            {
+                for (double i = dist1.Mean; i < dist2.Mean; i += 0.001)
+                {
+                    if (Math.Abs(dist1.Density(i) - dist2.Density(i)) < 0.00001)
                     {
-                        x = i;
+                        commonPoint = i;
                         found = true;
                         break;
                     }
                 }
-                if (found)
+            }
+            else
+            {
+                for (double i = dist2.Mean; i < dist1.Mean; i += 0.001)
                 {
-                    for (double i = x; i < x + (8 * gen2.StdDev); i += 0.1)
+                    if (Math.Abs(dist1.Density(i) - dist2.Density(i)) < 0.00001)
                     {
-                        sum += 0.1 * gen2.Density(i);
-                    }
-
-                    for (double i = x - (4 * gen1.StdDev); i < x; i += 0.1)
-                    {
-                        sum1 += 0.1 * gen1.Density(i);
+                        commonPoint = i;
+                        found = true;
+                        break;
                     }
                 }
             }
+            return found;
+        }
+
+        // Calculate integral of the intersection beetwen distributions
+        private double CalculateIntegral(IContinuousDistribution dist1,IContinuousDistribution dist2,double commonPoint,double p1,double p2)
+        {
+
+            double sum = 0.0;
+            double sum1 = 0.0;
+            for (double i = commonPoint; i < dist1.Mean + (4 * dist1.StdDev); i += 0.1)
+            {
+                sum += 0.1 * dist1.Density(i);
+            }
+
+            for (double i = commonPoint - (4 * dist2.StdDev); i < commonPoint; i += 0.1)
+            {
+                sum1 += 0.1 * dist2.Density(i);
+            }
+
             return (p1 * sum + p2 * sum1) * 100;
         }
     }
